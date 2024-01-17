a21471fefb206638414ec9bb32556f3b1e1340f7
MillanCV <m.castrovilarino@gmail.com>
2024-01-17 07:53:44 +0100

    correct exercise 1.4

--- src/exercises/part_1/course_information/src/App.jsx
+++ src/exercises/part_1/course_information/src/App.jsx
@@ -17,16 +17,16 @@ const Content = (props) => {
   console.log(props)
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
@@ -50,8 +50,8 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
 
 
     </>
