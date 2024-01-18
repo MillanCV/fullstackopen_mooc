913581c30d466abba26665242ecbaabd6832cb77
MillanCV <m.castrovilarino@gmail.com>
2024-01-18 17:47:36 +0100

    part1d update of the state is asynchronous

--- src/part_1d/part_1d_code/src/App.jsx
+++ src/part_1d/part_1d_code/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 const App = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0 })
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
@@ -10,6 +11,8 @@ const App = () => {
       ...clicks,
       left: clicks.left + 1,
     })
+
+    setTotal(clicks.left + clicks.right)
   }
 
   const handleRightClick = () => {
@@ -18,6 +21,7 @@ const App = () => {
       ...clicks,
       right: clicks.right + 1
     })
+    setTotal(clicks.left + clicks.right)
   }
 
   return (
@@ -31,6 +35,7 @@ const App = () => {
       </button>
       {clicks.right}
       <p>{allClicks.join(' ')}</p>
+      <p>Total clicks: {total}</p>
     </div>
   )
 }
