a1d758b27104e2aa34229e5cfd362af6bc597c4c
MillanCV <m.castrovilarino@gmail.com>
2024-01-19 06:54:38 +0100

    start unicafe exercises

--- src/part_1d/part_1d_code/src/App.jsx
+++ src/part_1d/part_1d_code/src/App.jsx
@@ -1,9 +1,33 @@
 import { useState } from "react"
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = props => <div > {props.value}</div>
+
 const App = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0 })
   const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
@@ -24,18 +48,23 @@ const App = () => {
     setTotal(clicks.left + clicks.right)
   }
 
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
   return (
     <div>
       {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+
       {clicks.right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
       <p>Total clicks: {total}</p>
+      <Button handleClick={setToValue(1000)} text="thousand" />
+      <Button handleClick={setToValue(0)} text="reset" />
+      <Button handleClick={setToValue(value + 1)} text="increment" />
     </div>
   )
 }
