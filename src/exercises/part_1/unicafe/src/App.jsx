81dedb98d8bf4bda8946623295d602350cc82221
MillanCV <m.castrovilarino@gmail.com>
2024-01-22 07:37:43 +0100

    complete exercise 1.12

--- src/exercises/part_1/unicafe/src/App.jsx
+++ src/exercises/part_1/unicafe/src/App.jsx
@@ -38,8 +38,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
-
   const setValue = (value, setValue) => () => setValue(value + 1)
 
   return (
